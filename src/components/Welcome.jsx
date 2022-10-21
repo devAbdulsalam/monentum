8111898fb55c015d4fbba1d137a9eca47749a21f
my-amtech <ammuftau74@gmail.com>
2022-10-21 11:21:15 +0100

    edit icons

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -65,9 +65,9 @@ const Welcome = () => {
                     <div className="text-center p-6 bg-gray-500 font-semibold">
                       <span>© <span>{new Date().getFullYear().toString()}</span> Copyright : </span>
                       <a  className="text-blue-900 font-semibold" href="https://devabdulsalam.netlify.app" target="_blank" rel='noreferrer'>Dev.Abdulsalm.js</a>
-                      <a href="https://twitter.com/MMAbdulsalam001" target="_blank" rel='noreferrer' className="mx-2 twitter"><i className="text-blue-700 fa fa fa-twitter "></i></a>
-                      <a href="https://github.com/devAbdulsalam" target="_blank" rel='noreferrer' className="mx-2 github"><i className="fa fa fa-github"></i></a>
-                      <a href="https://www.linkedin.com/in/abdulsalammmuftua/" target="_blank" rel='noreferrer' className="mx-2 text-white linkedin"><i className="fa fa fa-linkedin"></i></a>
+                      <a href="https://twitter.com/MMAbdulsalam001" target="_blank" rel='noreferrer' className="mx-2 twitter"><i className="text-blue-700 fas fa fa-twitter "></i></a>
+                      <a href="https://github.com/devAbdulsalam" target="_blank" rel='noreferrer' className="mx-2 github"><i className="fas fa fa-github"></i></a>
+                      <a href="https://www.linkedin.com/in/abdulsalammmuftua/" target="_blank" rel='noreferrer' className="mx-2 text-white linkedin"><i className="fas fa fa-linkedin"></i></a>
                     </div>
                 </div>
               </motion.div>
